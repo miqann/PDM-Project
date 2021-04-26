6507e0943727a0029b4ed86841fe68f102938f2a
MiQan <53591126+miqann@users.noreply.github.com>
2021-04-26 11:16:41 +0700

    Some test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import LoginPage from "./components/LoginPage/LoginPage";
-import { BrowserRouter as Router } from 'react-router-dom';
-
-
+import LoginPage from "./pages/Login";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
 
 function App() {
-    return(
+    return (
         <Router>
-            <LoginPage />
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/home">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/register">Dashboard</Link>
+                    </li>
+                </ul>
+
+                <Switch>
+                    <Route exact path="/login">
+                        
+                        <LoginPage />
+                    </Route>
+                    {/* <Route path="/about"> */}
+                    {/* <About /> */}
+                    {/* </Route> */}
+                    {/* <Route path="/dashboard"> */}
+                    {/* <Dashboard /> */}
+                    {/* </Route> */}
+                </Switch>
+
+            </div>
         </Router>
     );
 }

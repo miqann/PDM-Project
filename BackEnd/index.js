7e154951a8e8dfc0789bebda2fa79aa332773590
Laughing6901 <vandat96tn@gmail.com>
2021-05-06 09:16:28 +0700

    finish hotfix

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -10,7 +10,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', usersRouter);
+app.use('/user/:id', function (req, res, next) {
+  console.log('Request Type:', req.method);
+  console.log('Request URL:', req.originalUrl);
+  next()
+},function (req, res, next) {
+  console.log('Request Type:', req.method)
+  next()
+})
+
+app.get('/user/:id', function (req, res, next) {
+  res.send('USER')
+})
+// app.use('/users', usersRouter);
 
   app.listen(port, () => {
       console.log(`Example app listening at http://localhost:${port}`)

b408ac8fdd399e911d130a3fa5693a36c8586f28
Laughing6901 <vandat96tn@gmail.com>
2021-05-13 11:39:14 +0700

    last update cusModel

--- BackEnd/Models/CustomerModel.js
+++ BackEnd/Models/CustomerModel.js
@@ -6,11 +6,19 @@ const Customer = db.define('Customer',{
     CustomerId: {
         type: DataTypes.INTEGER,
         autoIncrement:true,
+        isInt: true,
+        len: [9, 10],
         primaryKey: true,
     },
     CustomerName: { type: DataTypes.STRING(40) },
-    CustomerPhone: {type: DataTypes.INTEGER},
-    CustomerEmail: {type: DataTypes.STRING(40)},
+    CustomerPhone: { 
+        type: DataTypes.INTEGER,
+        len: [10],
+    },
+    CustomerEmail: {
+        type: DataTypes.STRING(40),
+        isEmail: true,
+    },
     City: {type: DataTypes.STRING(40)},
 }, {
     timestamps: false,

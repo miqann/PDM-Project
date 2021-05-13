2bcfb847e34a7bd3a183f13b25a2be493d601e21
Laughing6901 <vandat96tn@gmail.com>
2021-05-13 18:23:58 +0700

    create LoginModels

---
+++ BackEnd/Models/LoginModels/LoginLog.js
@@ -0,0 +1,32 @@
+const { Sequelize, DataTypes, Model } = require('Sequelize');
+const db = require('../config/database');
+
+const LoginLog = db.define('LoginLog', {
+    LoginId: {
+        type: DataTypes.INTEGER,
+        autoIncrement:true, 
+        isInt: true,
+        allowNull: false,
+        len:[10],
+        primaryKey: true,
+    },
+    Times: {
+        type: DataTypes.DATE,
+    },
+    PlaceLogin: {
+        type: DataTypes.STRING(40),
+    },
+    Description: {
+        type:DataTypes.STRING(40),
+    },
+    Login: {
+        type: DataTypes.STRING(40),
+    },
+    Password: {
+        type: DataTypes.STRING(40),
+    }
+    },{
+        timestamps: false,
+});
+
+module.exports = LoginLog;

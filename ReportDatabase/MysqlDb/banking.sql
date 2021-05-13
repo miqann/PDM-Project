af9f37e374935964a2f520f2a17b3606a15856f1
Laughing6901 <vandat96tn@gmail.com>
2021-05-13 20:56:27 +0700

    midified sql

--- ReportDatabase/MysqlDb/banking.sql
+++ ReportDatabase/MysqlDb/banking.sql
@@ -1,5 +1,29 @@
+create database banking;
+use banks;
+CREATE TABLE `TransactionLogs` (
+  `TransactionId` int PRIMARY KEY NOT NULL,
+  `TransactionDate` datetime DEFAULT NULL,
+  `TransactionTypesId` int DEFAULT NULL,
+  `TransactionAmount` int DEFAULT NULL,
+  `AccountId` int DEFAULT NULL,
+  `TransactionTypeId` int DEFAULT NULL,
+  `TransactionStatusId` int DEFAULT NULL
+);
+
+CREATE TABLE `TransactionStatus` (
+  `TransactionStatusId` int PRIMARY KEY NOT NULL,
+  `TransactionStatusName` varchar(40) DEFAULT NULL
+);
+
+CREATE TABLE `TransactionTypes` (
+  `TransactionTypesId` int PRIMARY KEY NOT NULL,
+  `TransactionTypesName` varchar(40) DEFAULT NULL,
+  `TransactionTypesDescription` varchar(40) DEFAULT NULL,
+  `TransactionFee` int DEFAULT NULL
+);
+
 CREATE TABLE `Accounts` (
-  `AccountId` int NOT NULL,
+  `AccountId` int PRIMARY KEY NOT NULL,
   `CustomerId` int DEFAULT NULL,
   `AccountName` varchar(40) DEFAULT NULL,
   `DateOpened` date DEFAULT NULL,
@@ -7,95 +31,84 @@ CREATE TABLE `Accounts` (
   `Login` varchar(40) DEFAULT NULL,
   `Password` varchar(40) DEFAULT NULL,
   `AccountTypesCode` int DEFAULT NULL,
-  `AccountStatusId` int DEFAULT NULL,
-  PRIMARY KEY (`AccountId`),
-  KEY `CustomerId` (`CustomerId`),
-  KEY `AccountTypesCode` (`AccountTypesCode`),
-  KEY `AccountStatusId` (`AccountStatusId`),
-  CONSTRAINT `Accounts_ibfk_1` FOREIGN KEY (`CustomerId`) REFERENCES `Customers` (`CustomerId`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `Accounts_ibfk_2` FOREIGN KEY (`AccountTypesCode`) REFERENCES `AccountTypes` (`AccountTypesCode`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `Accounts_ibfk_3` FOREIGN KEY (`AccountStatusId`) REFERENCES `AccountStatus` (`AccountStatusId`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `AccountStatus` (
-  `AccountStatusId` int NOT NULL,
-  `AccountStatusDescription` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`AccountStatusId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `AccountTypes` (
-  `AccountTypesCode` int NOT NULL,
-  `AccountTypesDescription` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`AccountTypesCode`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `AccountStatusId` int DEFAULT NULL
+);
 
 CREATE TABLE `Customers` (
-  `CustomerId` int NOT NULL,
+  `CustomerId` int PRIMARY KEY NOT NULL,
   `CustomerName` varchar(40) DEFAULT NULL,
   `CustomerPhone` int DEFAULT NULL,
   `CustomerEmail` varchar(40) DEFAULT NULL,
   `city` varchar(40) DEFAULT NULL,
-  `CustomerTypesCode` int DEFAULT NULL,
-  PRIMARY KEY (`CustomerId`),
-  KEY `CustomerTypesCode` (`CustomerTypesCode`),
-  CONSTRAINT `Customers_ibfk_1` FOREIGN KEY (`CustomerTypesCode`) REFERENCES `CustomerTypes` (`CustomerTypesCode`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `CustomerTypesCode` int DEFAULT NULL
+);
 
 CREATE TABLE `CustomerTypes` (
-  `CustomerTypesCode` int NOT NULL,
-  `CustomerTypesDescription` varchar(40) DEFAULT NULL,
-  PRIMARY KEY (`CustomerTypesCode`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `CustomerTypesCode` int PRIMARY KEY NOT NULL,
+  `CustomerTypesDescription` varchar(40) DEFAULT NULL
+);
+
+
+CREATE TABLE `AccountStatus` (
+  `AccountStatusId` int PRIMARY KEY NOT NULL,
+  `AccountStatusDescription` varchar(50) DEFAULT NULL
+);
+
+CREATE TABLE `AccountTypes` (
+  `AccountTypesCode` int PRIMARY KEY NOT NULL,
+  `AccountTypesDescription` varchar(50) DEFAULT NULL
+);
 
 CREATE TABLE `LoginLog` (
-  `LoginId` int NOT NULL AUTO_INCREMENT,
+  `LoginId` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `Times` datetime DEFAULT NULL,
   `PlaceLogin` varchar(40) DEFAULT NULL,
   `Descriptions` varchar(40) DEFAULT NULL,
   `AccountId` int DEFAULT NULL,
   `LoginStatusId` int DEFAULT NULL,
   `Login` varchar(40) DEFAULT NULL,
-  `Password` varchar(40) DEFAULT NULL,
-  PRIMARY KEY (`LoginId`),
-  KEY `AccountId` (`AccountId`),
-  KEY `LoginStatusId` (`LoginStatusId`),
-  CONSTRAINT `LoginLog_ibfk_1` FOREIGN KEY (`AccountId`) REFERENCES `Accounts` (`AccountId`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `LoginLog_ibfk_2` FOREIGN KEY (`LoginStatusId`) REFERENCES `LoginStatus` (`LoginStatusId`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `Password` varchar(40) DEFAULT NULL
+);
 
 CREATE TABLE `LoginStatus` (
-  `LoginStatusId` int NOT NULL,
-  `LoginStatusName` varchar(40) DEFAULT NULL,
-  PRIMARY KEY (`LoginStatusId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `LoginStatusId` int PRIMARY KEY NOT NULL,
+  `LoginStatusName` varchar(40) DEFAULT NULL
+);
 
-CREATE TABLE `TransactionLogs` (
-  `TransactionId` int NOT NULL,
-  `TransactionDate` datetime DEFAULT NULL,
-  `TransactionTypesId` int DEFAULT NULL,
-  `TransactionAmount` int DEFAULT NULL,
-  `AccountId` int DEFAULT NULL,
-  `TransactionTypeId` int DEFAULT NULL,
-  `TransactionStatusId` int DEFAULT NULL,
-  PRIMARY KEY (`TransactionId`),
-  KEY `TransactionTypesId` (`TransactionTypesId`),
-  KEY `AccountId` (`AccountId`),
-  KEY `TransactionStatusId` (`TransactionStatusId`),
-  CONSTRAINT `TransactionLogs_ibfk_1` FOREIGN KEY (`TransactionTypesId`) REFERENCES `TransactionTypes` (`TransactionTypesId`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `TransactionLogs_ibfk_2` FOREIGN KEY (`AccountId`) REFERENCES `Accounts` (`AccountId`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `TransactionLogs_ibfk_3` FOREIGN KEY (`TransactionStatusId`) REFERENCES `TransactionStatus` (`TransactionStatusId`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `TransactionStatus` (
-  `TransactionStatusId` int NOT NULL,
-  `TransactionStatusName` varchar(40) DEFAULT NULL,
-  PRIMARY KEY (`TransactionStatusId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `TransactionTypes` (
-  `TransactionTypesId` int NOT NULL,
-  `TransactionTypesName` varchar(40) DEFAULT NULL,
-  `TransactionTypesDescription` varchar(40) DEFAULT NULL,
-  `TransactionFee` int DEFAULT NULL,
-  PRIMARY KEY (`TransactionTypesId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+ALTER TABLE `Accounts` ADD FOREIGN KEY (`CustomerId`) REFERENCES `Customers` (`CustomerId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `Accounts` ADD FOREIGN KEY (`AccountTypesCode`) REFERENCES `AccountTypes` (`AccountTypesCode`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `Accounts` ADD FOREIGN KEY (`AccountStatusId`) REFERENCES `AccountStatus` (`AccountStatusId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `Customers` ADD FOREIGN KEY (`CustomerTypesCode`) REFERENCES `CustomerTypes` (`CustomerTypesCode`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `LoginLog` ADD FOREIGN KEY (`AccountId`) REFERENCES `Accounts` (`AccountId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `LoginLog` ADD FOREIGN KEY (`LoginStatusId`) REFERENCES `LoginStatus` (`LoginStatusId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `TransactionLogs` ADD FOREIGN KEY (`TransactionTypesId`) REFERENCES `TransactionTypes` (`TransactionTypesId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `TransactionLogs` ADD FOREIGN KEY (`AccountId`) REFERENCES `Accounts` (`AccountId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `TransactionLogs` ADD FOREIGN KEY (`TransactionStatusId`) REFERENCES `TransactionStatus` (`TransactionStatusId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+CREATE INDEX `CustomerId` ON `Accounts` (`CustomerId`);
+
+CREATE INDEX `AccountTypesCode` ON `Accounts` (`AccountTypesCode`);
+
+CREATE INDEX `AccountStatusId` ON `Accounts` (`AccountStatusId`);
+
+CREATE INDEX `CustomerTypesCode` ON `Customers` (`CustomerTypesCode`);
+
+CREATE INDEX `AccountId` ON `LoginLog` (`AccountId`);
+
+CREATE INDEX `LoginStatusId` ON `LoginLog` (`LoginStatusId`);
+
+CREATE INDEX `TransactionTypesId` ON `TransactionLogs` (`TransactionTypesId`);
+
+CREATE INDEX `AccountId` ON `TransactionLogs` (`AccountId`);
+
+CREATE INDEX `TransactionStatusId` ON `TransactionLogs` (`TransactionStatusId`);

968d616bbc42c4d18980aa5396e18588510d4431
maipham2571@gmail.com <maipham2571@gmail.com>
2021-05-15 01:28:02 +0700

    Done UI of LoginPage + Register

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,7 +1,6 @@
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import Main from './components/Main';
-import LoginPage from './components/LoginPage';
 
 function App() {
     return(

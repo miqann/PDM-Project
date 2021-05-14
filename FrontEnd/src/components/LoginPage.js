3aed9b7da7e04df3214eb1448e9c843f73abcb2f
maipham2571@gmail.com <maipham2571@gmail.com>
2021-05-14 23:57:46 +0700

    checkout page

--- FrontEnd/src/components/LoginPage.js
+++ FrontEnd/src/components/LoginPage.js
@@ -16,26 +16,26 @@ class LoginPage extends Component {
     render (){
         return (
             <div class="bodyRegister">
-            <div class="main">
-            <div class="logo"></div>
-            <div class="title">Login Page</div>
-            <form onSubmit={this.handleSubmit}>
-                <div class="credentials">
-                    <div class="username">
-                        <UserOutlined />
-                        <input type="text" name="username" placeholder="Username" required="true"></input>
-                    </div>
-                    <div class="password">
-                        <LockOutlined />
-                        <input type="password" name="password" placeholder="Password" required="true"></input>
+                <div class="main">
+                    <div class="logo"></div>
+                    <div class="title">Login Page</div>
+                    <form onSubmit={this.handleSubmit}>
+                        <div class="credentials">
+                            <div class="username">
+                                <UserOutlined />
+                                <input type="text" name="username" placeholder="Username" required="true"></input>
+                            </div>
+                            <div class="password">
+                                <LockOutlined />
+                                <input type="password" name="password" placeholder="Password" required="true"></input>
+                            </div>
+                        </div>
+                        <button type="submit" class="submit">Login</button>
+                    </form>
+                    <div class="link">
+                        <a href="#">Forget your password? Contact us.</a> 
                     </div>
                 </div>
-                <button type="submit" class="submit">Login</button>
-            </form>
-            <div class="link">
-                <a href="#">Forget your password? Contact us.</a> 
-            </div>
-            </div>
             </div>
             );
     }
--- FrontEnd/src/components/RegisterPage.js
+++ FrontEnd/src/components/RegisterPage.js
@@ -16,38 +16,40 @@ class RegisterPage extends Component {
     render (){
         return (
             <div class="bodyRegister">
-            <div class="main">
-            <div class="logo"></div>
-            <div class="title">Register Page</div>
-            <form onSubmit={this.handleSubmit}>
-                <div class="credentials">
-                    <div class="username">
-                        <UserOutlined />
-                        <input type="text" name="username" placeholder="Username" required="true"></input>
-                    </div>
-                    <div class="phone">
-                        <PhoneOutlined />
-                        <input type="text" name="phone" placeholder="Phone number" required="true"></input>
-                    </div>
-                    <div class="email">
-                        <MailOutlined />
-                        <input type="text" name="email" placeholder="Email address" required="true"></input>
-                    </div>
-                    <div class="city">
-                        <HomeOutlined />
-                        <input type="text" name="city" placeholder="Your city" required="true"></input>
-                    </div>
-                    <div class="password">
-                        <LockOutlined />
-                        <input type="password" name="password" placeholder="Password" required="true"></input>
+                <div class="main">
+                    <div class="logo"></div>
+                    <div class="title">Register Page</div>
+                    <form onSubmit={this.handleSubmit}>
+                        <div class="credentials">
+                            <div class="username">
+                                <UserOutlined />
+                                    <input type="text" name="username" placeholder="Username" required="true"></input>
+                            </div>
+                            <div class="phone">
+                                <PhoneOutlined />
+                                    <input type="text" name="phone" placeholder="Phone number" required="true"></input>
+                            </div>
+                            <div class="email">
+                                <MailOutlined />
+                                    <input type="text" name="email" placeholder="Email address" required="true"></input>
+                            </div>
+                            <div class="city">
+                                <HomeOutlined />
+                                    <input type="text" name="city" placeholder="Your city" required="true"></input>
+                            </div>
+                            <div class="password">
+                                <LockOutlined />
+                                    <input type="password" name="password" placeholder="Password" required="true"></input>
+                            </div>
+                        </div>
+                        <button type="submit" class="submit">Register</button>
+                    </form>
+                    <div class="link">
+                        <a class="distance" href="#">Back</a>
+                        <a href="#">Need help? Contact us.</a> 
+                        
                     </div>
                 </div>
-                <button type="submit" class="submit">Register</button>
-            </form>
-            <div class="link">
-                <a href="#">Need help? Contact us.</a> 
-            </div>
-            </div>
             </div>
             );
     }

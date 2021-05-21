3da6bbae442ccc99b48cb7b6fc5bf2a431b74809
maipham2571@gmail.com <maipham2571@gmail.com>
2021-05-22 01:25:44 +0700

    Modify Transfer

--- FrontEnd/src/components/UserInterface.js
+++ FrontEnd/src/components/UserInterface.js
@@ -13,9 +13,15 @@ class UserInterface extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            AccountId : this.props.location.state.AccountId,//get data from login
+            //get data from login
+            // AccountId : this.props.location.state.AccountId,
+            
         }
+        
     }
+
+    
+
     render(){
         return(
             <div style={{ backgroundImage: `url(${Background})` }} class="wrapper "> 
@@ -25,33 +31,78 @@ class UserInterface extends Component{
                         <Link class=" brand" to="./">MDQT BANK</Link>
                         
                             
+                    <div class="wrapper-2">
+                        
+                        <div class="sidebar">
+                            <div>
+                                <ul class="bar-head">
+                                    {/* <li class="font">{this.state.AccountId}</li> */}
+                                    <img class="account-icon" src={Account}></img>
+
+                                </ul>
+                                <ul class="bar-element">
+                                    <li class="nav-item sidebar-elem ">My Wallet</li>
+                                    <img class="icon" src={Wallet}></img>
+                                </ul>
+
+                                <ul class="bar-element">
+                                    <li class="nav-item sidebar-elem">Transfer</li>
+                                    <img class="icon" src={Transfer}></img>
+
+                                </ul>
+                                <ul class="bar-element bar-stock">
+                                    <li class="nav-item sidebar-elem">Stock Exchange</li>
+                                    <img class="icon icon-stock" src={Money}></img>
+                                </ul>
+                                <ul class="bar-long-element" >
+                                    <li class="nav-item"></li>
+                                </ul>
+                                <ul >
+                                    <li class="nav-item elem">Contact Us</li>
+                                    <img class="sc-icon" src={SocialMedia}></img>
+                                </ul>
+                                </div>
+  
+                            </div>
+
+                           
+                            <div class ="container-small">
+                                <div class="cover">
+                                    <ul class ="text-big">Transferer Information
+                                        <li class="text-small">Account ID:</li>
+                                        <input class="distance" type="text"></input>
+                                        <span class="text-small">Available Balances:</span>
+                                        <span class="text-small">123.456 VND</span>
+                                    </ul>
+                                    
+                                    <ul class ="text-big">Receiver Information
+                                        <li class="text-small">Account ID:</li>
+                                        <input class="distance" type="text"></input>
+                                        <li class="text-small">Beneficiary Bank:</li>
+                                        <input class="distance" type="text"></input>
+                                    </ul>
+        
+                                    <ul class ="text-big">Transaction Information
+                                        <li class="text-small">Amount:</li>
+                                        <input class="distance" type="text"></input>
+                                        <li class="text-small">Transaction fee:</li>
+                                        <input class="distance" type="text"></input>
+                                        <li class="text-small">Note:</li>
+                                        <input class="distance" type="text"></input>
+                                    </ul>
+
+                                    <div class=" distance justify-content-md-end">
+                                        <button class="btn btn-primary " type="submit" value="Continue">Continue</button>
+                                    </div>
+                                </div>
+                                
+                            </div> 
+                        
+
+                        </div>
+
                     
-                    <div class="sidebar">
-                        <ul class="bar-head">
-                            <li class="font">{this.state.AccountId}</li>
-                            <img class="account-icon" src={Account}></img>
-            
-                        </ul>
-                        <ul class="bar-element">
-                            <li class="nav-item sidebar-elem ">My Wallet</li>
-                            <img class="icon" src={Wallet}></img>
-                        </ul>
-                        <ul class="bar-element">
-                            <li class="nav-item sidebar-elem">Transfer</li>
-                            <img class="icon" src={Transfer}></img>
-                        </ul>
-                        <ul class="bar-element bar-stock">
-                            <li class="nav-item sidebar-elem">Stock Exchange</li>
-                            <img class="icon icon-stock" src={Money}></img>
-                        </ul>
-                        <ul class="bar-long-element" >
-                            <li class="nav-item"></li>
-                        </ul>
-                        <ul >
-                            <li class="nav-item elem">Contact Us</li>
-                            <img class="sc-icon" src={SocialMedia}></img>
-                        </ul>
-                    </div>
+                        
                 </div>
             </div>    
 
